b19e6b8705871da8bffd4cc403edd94ff244185d
Panagiwtis.geor@gmail.com <Panagiwtis.geor@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-11-18 20:30:44 +0000

    Version 0.1

---
+++ branches/dev-issue-3/mydiarybook/src/controller/NewEntryController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import ui.NewEntryView2;
+
+/**
+ *
+ * @author Zarc
+ */
+public class NewEntryController {
+    
+    private NewEntryView2 theView = new NewEntryView2();
+    private 
+    NewEntryController(NewEntryView2 theView)
+    {
+        
+    }
+    
+}
