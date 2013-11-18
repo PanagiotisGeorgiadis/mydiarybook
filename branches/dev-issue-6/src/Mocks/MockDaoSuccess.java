5d711192230b378d65c39714c8fb08f8b633a68c
giovanny.johnny.20@gmail.com <giovanny.johnny.20@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-11-18 18:11:41 +0000

    The right names for user story 5

--- branches/dev-issue-6/src/Mocks/MockDaoSuccess.java
+++ branches/dev-issue-6/src/Mocks/MockDaoSuccess.java
@@ -13,11 +13,12 @@ import Controller.IDaoController;
  * @author JohnN'y
  */
 public class MockDaoSuccess implements IDaoController {
-
+    
     @Override
     public String displayEntry(MockEntry mockObj) {
         String text = mockObj.getEntry();
         return text;
     }
+    
     
 }
