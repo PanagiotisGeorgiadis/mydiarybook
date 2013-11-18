1f331a9ef35b17bd426bcc105df35f28bf376028
giovanny.johnny.20@gmail.com <giovanny.johnny.20@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-11-18 22:19:24 +0000

    right version 0.1

---
+++ branches/dev-issue-5./MyDiaryProject/src/EditEntries/TextAreaDemo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package EditEntries;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author JohnN'y
+ */
+public class TextAreaDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       TextAreaFrame textAreaFrame = new TextAreaFrame();
+        textAreaFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textAreaFrame.setSize(425,200);
+        textAreaFrame.setVisible(true);
+    }
+    
+}
