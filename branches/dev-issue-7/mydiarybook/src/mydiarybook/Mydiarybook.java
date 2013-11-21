fe03e0fde6bece9b55a32923813d7e97a783acb1
alexpatsanis@gmail.com <alexpatsanis@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-11-21 00:17:48 +0000

    issue for personal goal.

---
+++ branches/dev-issue-7/mydiarybook/src/mydiarybook/Mydiarybook.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mydiarybook;
+
+import javax.swing.JOptionPane;
+import ui.PersonalGoalForm;
+
+/**
+ *
+ * @author Zarc
+ */
+public class MyDiaryBook {
+    
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO refer to https://code.google.com/p/mydiarybook/issues/list
+        
+        //JOptionPane.showMessageDialog(null, "Hello World!");
+        PersonalGoalForm form = new PersonalGoalForm();
+        
+        
+        form.setVisible(true);
+    }
+    
+}
