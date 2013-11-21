5d22b2183f3cb6f4e166c205f6439ddb48c8b5d1
alexpatsanis@gmail.com <alexpatsanis@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-11-21 16:47:32 +0000

    update

--- trunk/src/ui/DiaryForm.java
+++ trunk/src/ui/DiaryForm.java
@@ -90,7 +90,7 @@ public class DiaryForm extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void closeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_closeButtonActionPerformed
-
+       this.dispose();
     }//GEN-LAST:event_closeButtonActionPerformed
 
     /**
