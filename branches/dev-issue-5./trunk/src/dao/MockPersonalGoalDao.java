e94b48a254256a560c1c33089f5d0e9c655278b1
giovanny.johnny.20@gmail.com <giovanny.johnny.20@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-11-23 18:42:37 +0000

    version 0.1

---
+++ branches/dev-issue-5./trunk/src/dao/MockPersonalGoalDao.java
@@ -0,0 +1,33 @@
+
+import dao.IPersonalGoalDao;
+import java.util.ArrayList;
+import java.util.List;
+import model.PersonalGoalModel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ *
+
+package dao;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.PersonalGoalModel;
+
+/**
+ * Ένα απλό mock για success απο Dao.
+ * @author alex
+ */
+public class MockPersonalGoalDao implements IPersonalGoalDao{
+    List<PersonalGoalModel> PersonalGoalModel = new ArrayList<PersonalGoalModel>();
+    
+    @Override
+ public boolean writeToFilePersonalGoal(PersonalGoalModel cr){
+     PersonalGoalModel.add(cr);
+     return true;
+ }
+    
+    
+}
