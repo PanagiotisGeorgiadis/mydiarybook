e6a708860c30abdbca97b0a296fbb8858b17f0c2
dimiswebs@gmail.com <dimiswebs@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-11-27 20:12:31 +0000

    re upload for compatibility

---
+++ branches/dev-issue-31/src/main/java/controller/NewEntryController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import ui.NewEntryView;
+
+/**
+ *
+ * @author Zarc
+ */
+public class NewEntryController {
+    
+    private NewEntryView theView = new NewEntryView();
+    private 
+    NewEntryController(NewEntryView theView)
+    {
+        
+    }
+    
+}
