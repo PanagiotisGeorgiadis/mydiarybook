3e33fba8269e3f3424cd6b5a2da5ab85a4403443
giovanny.johnny.20@gmail.com <giovanny.johnny.20@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-11-27 17:40:00 +0000

    v 0.1

---
+++ branches/dev-issue-6/src/main/java/ui/Importantmoments.java
@@ -0,0 +1,24 @@
+
+
+package ui;
+
+public class Importantmoments 
+{       
+    
+            public boolean CheckLocation(String location)
+            {
+                if(location.length() > 20 || location.length() <=2) return false;
+                return true;
+            }
+            
+            public boolean CheckTitle(String location)
+            {
+                if(location.length() > 20 || location.length() <=2) return false;
+                return true;
+            }
+        
+            
+    
+      
+    
+}
