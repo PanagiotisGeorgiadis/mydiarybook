1c9e61ef91d838997ae0033864f98f7c822bb896
Panagiwtis.geor@gmail.com <Panagiwtis.geor@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-12-05 16:29:06 +0000

    Merge from dev-issue-1-18-...

--- src/main/java/controller/RegisterController.java
+++ src/main/java/controller/RegisterController.java
@@ -4,6 +4,11 @@
  */
 package controller;
 
+import static ui.RegisterForm.jPasswordField1;
+import static ui.RegisterForm.jPasswordField2;
+import static ui.RegisterForm.jTextField1;
+import static ui.RegisterForm.jTextField2;
+
 /**
  *
  * @author user
@@ -13,21 +18,46 @@ public class RegisterController
     public boolean checkMail(String email)
     {
         
-        if(email.length() <= 2) return false;
-        if(!email.contains("@") || email.indexOf("@") == 0 || email.indexOf("@") == email.length()-1) return false;
+        if(email.length() <= 4) return false;
+        if(!email.contains("@") || email.indexOf("@") == 0 || email.indexOf("@") == email.length()-1 || email.contains("@@")) 
+            return false;
         return true;
     }
     public boolean checkUsername(String username)
     {
         if(username.length() > 20) return false;
         if(username.length() <= 2) return false;
+        if(username.contains(" ")) return false;
+                
         return true;
     }
     public boolean checkPassword(String password, String repeatedpassword)
     {
         if(!password.equals(repeatedpassword)) return false;
-        if(password.length() > 8 || password.length() < 2) return false;
+        if(password.length() > 12 || password.length() <= 2) return false;
+       if(password.contains(" ")) return false;
+        
+        if(password.matches("[0-9]+")==true) return false;
+            
         return true;
     }
-    
+
+    public static boolean checkValues() {
+        RegisterController e = new RegisterController();
+        if (!e.checkMail(jTextField2.getText())) {
+            return false;
+
+        }
+
+        if (!e.checkUsername(jTextField1.getText())) {
+            return false;
+        }
+        if (!e.checkPassword(jPasswordField2.getText(), jPasswordField1.getText())) {
+            return false;
+        }
+        return true;
+    }    
+
+
+
 }
--- src/main/java/ui/ResetPassword.java
+++ src/main/java/ui/ResetPassword.java
@@ -1,230 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package ui;
 
 import javax.swing.JOptionPane;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.DriverManager;
-import java.sql.Connection;
 import controller.ResetPasswordController;
+import static dao.ResetPasswordDao.A;
+import static dao.ResetPasswordDao.B;
+import static dao.ResetPasswordDao.C;
+import static dao.ResetPasswordDao.generateNewPassword;
+import static dao.ResetPasswordDao.getSecurityQuestions;
+import static dao.ResetPasswordDao.questionsok;
+import static dao.ResetPasswordDao.randomNum;
+import static dao.ResetPasswordDao.saveNewPassword;
 
 import java.awt.datatransfer.*;
 import java.awt.Toolkit;
-
-
+import static ui.ChangePassword.ok;
 
 /**
  *
  * @author w7user
  */
 public class ResetPassword extends javax.swing.JFrame {
-    String A="0",B="0",C="0"; 
-    int randomNum = 0;
-    String jdbcDriver = "com.mysql.jdbc.Driver";
-    String dbURL = "jdbc:mysql://89.163.172.10/tl";
-    String dbUserId = "tl";
-    String dbPassword = "tl";
-    boolean ok = true;
-    boolean questionsok = false;
-    String question1;
-    String question2;
-    int question3 = 0;
-    
-      Connection c = null;
-      Connection c2 = null;
-    /**
-     * Creates new form ResetPassword
-     */
+
+    public static boolean ok;
+
     public ResetPassword() {
         initComponents();
     }
-     public void saveNewPassword() {
 
-      try {    
-        Class.forName(jdbcDriver);
-      } catch (ClassNotFoundException exp) {
-        System.err.println("Could not load the JDBC driver " + jdbcDriver);
-        return;
-      }
-        
-      try {
-        c = DriverManager.getConnection(dbURL, dbUserId, dbPassword);
-                
-        try {
-            String currentuser=username.getText();
-            String newpass = B+A+C+randomNum+A+B+C;
-            String updateString =
-        "update accounts set password = '"+newpass+"' where username ='"+currentuser+"'";
-         PreparedStatement  s= c.prepareStatement(updateString);
-         
-        
-        
-                
-          // In case you want to see the update results from each statement
-          // Normally not needed to store the results.
-          s.execute();
-          	    
-	    s.close();
-             
-            
-          
-          
-        } catch (SQLException sqlexp) {
-            JOptionPane.showMessageDialog(null,"Failed to execute one of the statements."+sqlexp.getMessage());
 
-            ok =false;
-        }
-        
-        c.close();
-        
-      } catch (SQLException sqlexp) {
-          JOptionPane.showMessageDialog(null,"Failed to connect to the database."+sqlexp.getMessage());
-          ok=false;
-
-      }
-      
-      
-     }
-     public void generateNewPassword(){
-             randomNum = 99999 + (int)(Math.random()*999999);
-    
-    int randomNum2 =1 +(int)(Math.random()* 5);
-    if (randomNum2 == 1)
-    {
-        A = "C";
-        
-    }
-    else if (randomNum2 == 2)
-            {
-             A = "Y";   
-            }
-        else if (randomNum2 == 3)
-            {
-             A = "X";   
-            }
-        else if (randomNum2 == 4)
-            {
-             A = "J";   
-            }
-        else if (randomNum2 == 5)
-            {
-             A = "F";   
-            }
-    
-    int randomNum3 =1 +(int)(Math.random()* 5);
-        if (randomNum3 == 1)
-    {
-        B = "C";
-        
-    }
-    else if (randomNum3 == 2)
-            {
-             B = "Y";   
-            }
-        else if (randomNum3 == 3)
-            {
-             B = "X";   
-            }
-        else if (randomNum3 == 4)
-            {
-             B = "J";   
-            }
-        else if (randomNum3 == 5)
-            {
-             B = "F";   
-            }
-    int randomNum4 =1 +(int)(Math.random()* 5);
-        if (randomNum4 == 1)
-    {
-        C = "C";
-        
-    }
-    else if (randomNum4 == 2)
-            {
-             C = "Y";   
-            }
-        else if (randomNum4 == 3)
-            {
-             C = "X";   
-            }
-        else if (randomNum4 == 4)
-            {
-             C = "J";   
-            }
-        else if (randomNum4 == 5)
-            {
-             C = "F";   
-            }
-     }
-     public void getSecurityQuestions(){
-               try {    
-        Class.forName(jdbcDriver);
-      } catch (ClassNotFoundException exp) {
-        System.err.println("Could not load the JDBC driver " + jdbcDriver);
-        return;
-      }
-        
-      try {
-        c2 = DriverManager.getConnection(dbURL, dbUserId, dbPassword);
-                
-        try {
-            String currentuser2=username.getText();
-            question1=q1.getText();
-            question2=q2.getText();
-            String getQuestions1 ="SELECT * FROM accounts where username='"+currentuser2+"' and q1='"+question1+"' and q2='"+question2+"';";
-            PreparedStatement  s= c2.prepareStatement(getQuestions1);
-            ResultSet rset = s.executeQuery();
-            
-            
-             if (rset.next())
-            {
-                questionsok=true;
-            }
-
-            
-            s.close();
-            
-
-        
-        
-                
-      
-          
-           
-          	    
-	   
-             
-            
-          
-          
-        } catch (SQLException sqlexp) {
-           JOptionPane.showMessageDialog(null,"Failed to execute one of the statements."+sqlexp.getMessage());
-            ok =false;
-        }
-        
-        c2.close();
-        
-      } catch (SQLException sqlexp) {
-          JOptionPane.showMessageDialog(null,"Failed to connect to the database."+sqlexp.getMessage());
-          ok=false;
-
-      }
-         
-     }
-
-    /**
-     * This method is called from within the constructor to initialize the form.
-     * WARNING: Do NOT modify this code. The content of this method is always
-     * regenerated by the Form Editor.
-     */
-    @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
@@ -312,43 +116,32 @@ public class ResetPassword extends javax.swing.JFrame {
 
     private void submitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_submitActionPerformed
         // TODO add your handling code here:
-        if (!checkValues())
-    {
-        JOptionPane.showMessageDialog(null, "Some Of Your Answers Are Too Small!");
-        q1.setText(null);
-        q2.setText(null);
-        q1.requestFocus();
-    }
-        else
-        {
-      getSecurityQuestions();
-    
-    if (questionsok==false)
-    {
-        JOptionPane.showMessageDialog(null,"At Least One Of Your Answers Is Wrong!");
-        q1.requestFocus();
-        return;
-    }
+        if (!controller.ResetPasswordController.checkValues()) {
+            JOptionPane.showMessageDialog(null, "Some Of Your Answers Are Too Small!");
+            q1.setText(null);
+            q2.setText(null);
+            q1.requestFocus();
+        } else {
+            dao.ResetPasswordDao.getSecurityQuestions();
 
-            
+            if (dao.ResetPasswordDao.questionsok == false) {
+                JOptionPane.showMessageDialog(null, "At Least One Of Your Answers Is Wrong!");
+                q1.requestFocus();
+                return;
+            }
 
-    generateNewPassword(); 
-    questionsok=false;
-    
-    
-    
-    //update new pass in db
-    
-    saveNewPassword();
-    if (ok==true)
-    {
-    String myString = B+A+C+randomNum+A+B+C;
-    StringSelection stringSelection = new StringSelection (myString);
-    Clipboard clpbrd = Toolkit.getDefaultToolkit ().getSystemClipboard ();
-    clpbrd.setContents (stringSelection, null);    
-    JOptionPane.showMessageDialog(null,"New Pass Is " +B+A+C+randomNum+A+B+C+" And Autocopied To Clipboard");
-    }
-    
+            dao.ResetPasswordDao.generateNewPassword();
+            questionsok = false;
+
+            dao.ResetPasswordDao.saveNewPassword();
+            if (dao.RegisterFormDao.ok == true) {
+                String myString = B + A + C + randomNum + A + B + C;
+                StringSelection stringSelection = new StringSelection(myString);
+                Clipboard clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard();
+                clpbrd.setContents(stringSelection, null);
+                JOptionPane.showMessageDialog(null, "New Pass Is " + B + A + C + randomNum + A + B + C + " And Autocopied To Clipboard");
+            }
+
         }
     }//GEN-LAST:event_submitActionPerformed
 
@@ -356,8 +149,6 @@ public class ResetPassword extends javax.swing.JFrame {
         // TODO add your handling code here:
         dispose();
     }//GEN-LAST:event_closeActionPerformed
-    
-
 
     /**
      * @param args the command line arguments
@@ -394,27 +185,15 @@ public class ResetPassword extends javax.swing.JFrame {
         });
     }
 
-    
-    public boolean checkValues() {
-        ResetPasswordController e = new ResetPasswordController();
-       
-        
-      
-   
-        if (!e.ResetPasswordController(q1.getText(), q2.getText())) {
-            return false;
-        }
-       return true;
-     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton close;
-    private javax.swing.JTextField q1;
-    private javax.swing.JTextField q2;
+    public static javax.swing.JTextField q1;
+    public static javax.swing.JTextField q2;
     private javax.swing.JLabel question1label;
     private javax.swing.JLabel question1label1;
     private javax.swing.JButton submit;
-    private javax.swing.JTextField username;
+    public static javax.swing.JTextField username;
     private javax.swing.JLabel usernamelabel;
     // End of variables declaration//GEN-END:variables
 }
