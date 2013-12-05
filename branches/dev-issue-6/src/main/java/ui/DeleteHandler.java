c6a2e00d3f6fe86324451d4daf4f794c847bcea2
giovanny.johnny.20@gmail.com <giovanny.johnny.20@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-12-05 12:47:52 +0000

    Commiting changes of dev-issue-7

---
+++ branches/dev-issue-6/src/main/java/ui/DeleteHandler.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ui;
+
+import controller.DeleteController;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author JohnN'y
+ */
+
+public class DeleteHandler implements ActionListener {
+    
+       private DeleteController delObj = new DeleteController();
+       private File fileText = new File("mpal.txt");
+       private JFrame form = new JFrame();
+       
+/*
+    public DeleteHandler(File fileText) {
+    this.fileText = fileText;
+    }
+     */
+    public DeleteHandler(Delete form) {
+        this.form = form;
+        //8a prepei na dimiourgisoume mia function getFile() sti form pou pros8etetai to DeleteHandler, wste na mporoume na paroume to filText apo ti forma
+        fileText = form.getFile();
+    }
+       
+       
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        
+     int dialog =  JOptionPane.showConfirmDialog(form, "Are U sure that you want to delete it?");
+     
+     
+      if(dialog == JOptionPane.YES_OPTION){
+                
+        try{
+            if(delObj.checkFileExistance(fileText)){
+                    delObj.delete(fileText);
+                    JOptionPane.showMessageDialog(form, delObj.showSuccess(), "Success ", JOptionPane.DEFAULT_OPTION);
+            }                    
+            else
+              // throw NullPointerException;
+            JOptionPane.showMessageDialog(form, delObj.showNoFileFound(), "No File...", JOptionPane.DEFAULT_OPTION);
+            
+        }catch(NullPointerException exc){
+            
+            JOptionPane.showMessageDialog(form, delObj.showNoFileFound(), "No File...", JOptionPane.DEFAULT_OPTION);
+            
+        }catch(Exception exc){
+            
+            JOptionPane.showMessageDialog(form, delObj.showError(), "ERROR", JOptionPane.DEFAULT_OPTION);
+            
+        }
+        
+    }
+  
+    }
+    
+}
