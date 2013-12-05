265f3235c7ab06af6b36715bf125cc4989067f22
giovanny.johnny.20@gmail.com <giovanny.johnny.20@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-12-05 11:22:35 +0000

    v 0.1

---
+++ branches/dev-issue-6/src/main/java/dao/IDaoController.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dao;
+
+/**
+ *
+ * @author PTsilopoulos
+ */
+public interface IDaoController {
+    //FIX replace string to mockobject
+     String displayEntry(String text);
+    }
