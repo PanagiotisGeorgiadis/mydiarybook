78b9eb5fabc1a6b456a6db787a2cb25478015468
giovanny.johnny.20@gmail.com <giovanny.johnny.20@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-12-08 20:42:55 +0000

    The new branch for dev-issue-6

---
+++ branches/dev-issue-6a/src/main/java/dao/PersonalGoalPathDao.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dao;
+
+/**
+ * 
+ * @author alex
+ */
+public class PersonalGoalPathDao {
+    
+    public PersonalGoalPathDao()
+    {
+        
+    }
+    
+    public String DefaultPath()
+    {
+        return "hi";
+    }
+    public String UserName()
+    {
+        return "bye";
+    }
+}
