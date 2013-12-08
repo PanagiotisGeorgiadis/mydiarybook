cff6c4478bd5a053995d8dc962a683f84add7e80
mariosttass@gmail.com <mariosttass@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-12-08 21:27:11 +0000

    UI testing

---
+++ branches/dev-issue-34a/src/main/java/ui/INewEntryView.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui;
+
+import java.net.URI;
+
+/**
+ *
+ * @author Zarc
+ */
+public interface INewEntryView {
+
+    /**
+     *
+     * @param errorMessage is a string and its used to display the message I
+     * want to the user
+     */
+    void displayErrorMessage(String errorMessage);
+    
+
+    void displayNewImage(URI imagePath);
+
+    void displayVideo(String videoPath,String whatToDo);
+
+    /**
+     *
+     * @return a String which contains the Text the user Wrote
+     */
+    String getTextArea();
+    
+}
