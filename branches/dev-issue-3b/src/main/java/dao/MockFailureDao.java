1a1ed49723be3d4bf112af925d3312c7a3f16f0b
Panagiwtis.geor@gmail.com <Panagiwtis.geor@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-12-10 21:49:46 +0000

    Modifying folders

---
+++ branches/dev-issue-3b/src/main/java/dao/MockFailureDao.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+/**
+ *
+ * @author Zarc
+ */
+public class MockFailureDao {
+    
+    public boolean writeToDatabase()
+    {
+        return false;
+    }
+    
+}
