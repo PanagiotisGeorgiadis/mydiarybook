540c0636f5c73e38ae379118f0ae3a1c48c18d78
giovanny.johnny.20@gmail.com <giovanny.johnny.20@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-12-11 20:25:40 +0000

    v 0.3

---
+++ branches/dev-issue-6a/src/main/java/dao/MockFileExist.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dao;
+
+import java.io.File;
+
+/**
+ *
+ * @author JohnN'y
+ */
+public class MockFileExist implements IEntriesDao{
+    private String textFile = new String(System.getProperty("user.dir")+"\\Text");
+
+   
+   /* 
+     @Override
+    public String getTextFile() {
+        return textFile;
+    }
+*/
+    @Override
+    public String getFile() {
+        return  textFile;
+    }
+
+    @Override
+    public void setFile(String textFile) {
+        this.textFile = textFile;
+    }
+    
+}
