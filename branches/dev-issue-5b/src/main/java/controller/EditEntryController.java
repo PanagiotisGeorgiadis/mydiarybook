432d1aafd2b095833c924ccd50bc5e8d8f98b32c
EnlightnedGenesis@gmail.com <EnlightnedGenesis@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-12-12 15:37:10 +0000

    Version 0.4 - 12/12/2013 Controller tests added, UI constructor optimized for use with the DisplayForm.

--- branches/dev-issue-5b/src/main/java/controller/EditEntryController.java
+++ branches/dev-issue-5b/src/main/java/controller/EditEntryController.java
@@ -16,19 +16,36 @@ import model.EditEntry;
  */
 public class EditEntryController {
     
-    private IEditEntryDao EditEntryDao;
+    public EditEntry newEditEntry;
+    public IEditEntryDao EditEntryDao;
     public EditEntryController()
     {
         
     }
     public EditEntryController(IEditEntryDao EditEntryDao) {
         this.EditEntryDao = EditEntryDao;
+        this.newEditEntry = new EditEntry();
     }
-    
-    public EditEntry displayEntry(String entryObj){
-        if (entryObj == null)
-            throw new IllegalArgumentException("You forgot to add content to your entry!");
-        return new EditEntry();
+       
+    public boolean checkEdit(String textToEdit)
+    {
+        if(!("").equals(textToEdit.trim())) {
+        }
+        else {
+            return false;
+        }
+        return true;
     }
-    
 }
+
+/*public EditEntry displayEntry(String entryObj){
+        try {
+            if (!entryObj.equals(null))
+                newEditEntry.setEntry(entryObj);
+                return new EditEntry();    
+        }
+        catch (Exception e) {
+                //throw new IllegalArgumentException("You forgot to add content to your entry!");
+                return new EditEntry();
+        }
+    }*/
