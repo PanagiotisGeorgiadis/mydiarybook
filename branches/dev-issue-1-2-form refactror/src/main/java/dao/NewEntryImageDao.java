5a5a5a20e76e04daf5f2501e91ddae83ec9a2824
s.stevenidis@gmail.com <s.stevenidis@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2013-12-20 23:58:49 +0000

    ui refactor

---
+++ branches/dev-issue-1-2-form refactror/src/main/java/dao/NewEntryImageDao.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dao;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.List;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ *
+ * @author Zarc
+ */
+public class NewEntryImageDao {
+   
+    private final String fSeparator = File.separator;
+    
+    public NewEntryImageDao()
+    {
+    }
+    
+    public boolean prepareImageForCopy(String entryTitle,String sourcePath)
+    {
+        INewEntryMockDefaultPath rootPathMock = mock(INewEntryMockDefaultPath.class);
+        when(rootPathMock.getDefaultPath()).thenReturn(System.getProperty("user.dir")+fSeparator+"MyDiaryBook"+fSeparator+"Users"+fSeparator);
+        
+        INewEntryMockUsername userMock = mock(INewEntryMockUsername.class);
+        when(userMock.getUsername()).thenReturn("Panagiwtis Georgiadis");
+    
+        String imageDestPath = rootPathMock.getDefaultPath()+userMock.getUsername()+fSeparator+"Entries"+fSeparator+entryTitle+fSeparator+"Images"+fSeparator;
+        
+        File destFile = new File(imageDestPath);
+        File sourceFile = new File(sourcePath);        
+        FilesDao copyDao = new FilesDao();
+        
+        try
+        {
+            copyDao.copyFile(sourceFile,destFile);
+        }
+        catch(IOException e)
+        {
+            return false;
+        }
+        
+        return true;
+    }
+    
+    public String[] getImageList(String entryTitle)
+    {
+        INewEntryMockDefaultPath rootPathMock = mock(INewEntryMockDefaultPath.class);
+        when(rootPathMock.getDefaultPath()).thenReturn(System.getProperty("user.dir")+fSeparator+"MyDiaryBook"+fSeparator+"Users"+fSeparator);
+        
+        INewEntryMockUsername userMock = mock(INewEntryMockUsername.class);
+        when(userMock.getUsername()).thenReturn("Panagiwtis Georgiadis");
+    
+        String imagePath = rootPathMock.getDefaultPath()+userMock.getUsername()+fSeparator+"Entries"+fSeparator+entryTitle+fSeparator+"Images"+fSeparator;
+        
+        FilesDao imageDao = new FilesDao();
+        try{
+            return imageDao.getFilesList(imagePath);
+        }
+        catch(NullPointerException e)
+        {
+            String[] exception = null;
+            
+            return exception;
+        }
+    }
+    
+    public List<URI> getImageFiles(String entryTitle)
+    {
+        INewEntryMockDefaultPath rootPathMock = mock(INewEntryMockDefaultPath.class);
+        when(rootPathMock.getDefaultPath()).thenReturn(System.getProperty("user.dir")+fSeparator+"MyDiaryBook"+fSeparator+"Users"+fSeparator);
+        
+        INewEntryMockUsername userMock = mock(INewEntryMockUsername.class);
+        when(userMock.getUsername()).thenReturn("Panagiwtis Georgiadis");
+    
+        String imagePath = rootPathMock.getDefaultPath()+userMock.getUsername()+fSeparator+"Entries"+fSeparator+entryTitle+fSeparator+"Images"+fSeparator;
+        
+        FilesDao imageDao = new FilesDao();
+        try{
+            return imageDao.getSubFiles(imagePath);
+        }catch(NullPointerException ex){
+            return null;
+        }
+        
+    }
+    
+}
---
+++ branches/dev-issue-1-2-form refactror/src/test/java/controller/DeleteControllerTest.java
@@ -0,0 +1,150 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+import dao.MockFileExist;
+import dao.MockFileNoExist;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author JohnN'y
+ */
+public class DeleteControllerTest {
+
+    public DeleteControllerTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws IOException {
+        
+     Path path = Paths.get(System.getProperty("user.dir") + "\\Text\\");
+        Path path1;
+        File file = new File(path.toString());
+        file.mkdirs();
+        path1 = Paths.get(path + "\\text.txt");
+        file = new File(path1.toString());
+       
+        path = Paths.get(path + "\\text1.txt");
+        file = new File(path1.toString());
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+        
+    }
+
+    @Before
+    public void setUp()  {        
+        File file = new File("."+"\\Text\\");
+        file.mkdir();
+    }
+
+    @After
+    public void tearDown() {
+        Path path = Paths.get(System.getProperty("user.dir")+File.separator+"Text"+File.separator);
+        try {
+            Files.deleteIfExists(path);
+        } catch (IOException ex) {
+            Logger.getLogger(DeleteControllerTest.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    /**
+     * Test of checkFileExistance method, of class DeleteController.
+     */
+    @Test
+    public void testSuccessfulCheckFileExistance() {
+        System.out.
+                println("Success CheckFileExistance");
+        //File fileText = new File(System.getProperty("user.dir")+"\\Text");
+        File fileText = new File("."+ File.separator + "Text"+File.separator);
+        DeleteController instance = new DeleteController(new MockFileExist());
+        boolean expResult = true;
+        boolean result = instance.checkFileExistance(fileText);
+        assertEquals(expResult, result);
+    }
+    
+      @Test
+    public void testFailCheckFileExistance() {
+        System.out.println("Fail CheckFileExistance");
+        File fileText = new File(System.getProperty("user.dir")+"\\babis");
+        DeleteController instance = new DeleteController(new MockFileNoExist());
+        boolean expResult = false;
+        boolean result = instance.checkFileExistance(fileText);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of delete method, of class DeleteController.
+     */
+    @Test
+    public void testSuccessDelete() {
+        System.out.println("Successful delete");
+        File fileText = new File(System.getProperty("user.dir")+"\\Text");
+        DeleteController instance = new DeleteController(new MockFileExist());
+        boolean expResult = true;
+        boolean result = instance.delete(fileText);
+        assertEquals(expResult, result);
+    }
+
+        @Test
+    public void testFailDelete() {
+        System.out.println("Fail delete");
+        File fileText = new File("babis");
+        DeleteController instance = new DeleteController(new MockFileNoExist());
+        boolean expResult = false;
+        boolean result = instance.delete(fileText);
+        assertEquals(expResult, result);
+    }
+    /**
+     * Test of showNoFileFound method, of class DeleteController.
+     */
+    @Test
+    public void testShowNoFileFound() {
+        System.out.println("showNoFileFound");
+        DeleteController instance = new DeleteController(new MockFileExist());
+        String expResult = "There is not such a file";
+        String result = instance.showNoFileFound();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of showError method, of class DeleteController.
+     */
+    @Test
+    public void testShowError() {
+        System.out.println("showError");
+        DeleteController instance = new DeleteController(new MockFileExist());
+        String expResult = "There was  a undefinied error";
+        String result = instance.showError();
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of showSuccess method, of class DeleteController.
+     */
+    @Test
+    public void testShowSuccess() {
+        System.out.println("showSuccess");
+        DeleteController instance = new DeleteController(new MockFileExist());
+        String expResult = "Successful Erase";
+        String result = instance.showSuccess();
+        assertEquals(expResult, result);
+    }
+
+}
