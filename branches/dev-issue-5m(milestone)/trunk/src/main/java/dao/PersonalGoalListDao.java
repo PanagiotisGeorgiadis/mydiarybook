4a3269b43c2e38b8db0530e4110e3698bb6eedb9
EnlightnedGenesis@gmail.com <EnlightnedGenesis@gmail.com@f2e67ffa-b24b-c878-d42d-560373b34170>
2014-01-15 23:05:58 +0000

    Milestone Version of Edits.

---
+++ branches/dev-issue-5m(milestone)/trunk/src/main/java/dao/PersonalGoalListDao.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dao;
+
+import exception.EntryException;
+import java.io.File;
+import model.Login;
+
+/**
+ *
+ * @author nova
+ */
+public class PersonalGoalListDao {
+    String fSeparator = File.separator;
+    public String[] getListOfPersonalGoal()
+    {
+        String personalGoalPath = System.getProperty("user.dir") + fSeparator + "MyDiaryBook" + fSeparator + "Users" + fSeparator + Login.getUsername() + fSeparator + "PersonalGoal";
+        FilesDao personalGoalDao = new FilesDao();
+        try{
+            return personalGoalDao.getDirectoryList(personalGoalPath);
+        }catch(EntryException ex){
+            return null;
+        }
+        //check apo to FilesDaos to getDirectoryList an yparxei kapoio problima
+    }
+    
+}
